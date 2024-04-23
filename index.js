d18608347a4fce099530cc9369e42115b4c2c5bb
THANH <95860402+ccna123@users.noreply.github.com>
2024-04-23 10:49:08 +0900

    update

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ app.get("/getAllChar", async (req, res) => {
     console.log(error);
   }
 });
+app.get("/getAllSpells", async (req, res) => {
+  try {
+    const result = await axios.get(
+      "https://potterapi-fedeperin.vercel.app/en/spells"
+    );
+    return res.status(200).json(result.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen("4000", (req, res) => {
   console.log("Server listening on port 4000");
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,5 +12,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-npm install
+sudo npm install
 pm2 start index.js
