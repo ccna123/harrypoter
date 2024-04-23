b0334dc3b3f40d3d312a906dcf8bb66c0ebbd735
THANH <95860402+ccna123@users.noreply.github.com>
2024-04-23 11:13:59 +0900

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -10,6 +10,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-sudo npm install
+npm install
 npm install -g pm2
 pm2 start index.js
