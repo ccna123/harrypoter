21ca944ac7be27d101e2498a135f8e556f962fc0
THANH <95860402+ccna123@users.noreply.github.com>
2024-04-23 11:17:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ app.get("/getAllSpells", async (req, res) => {
     console.log(error);
   }
 });
+app.get("/getAllBooks", async (req, res) => {
+  try {
+    const result = await axios.get(
+      "https://potterapi-fedeperin.vercel.app/en/books"
+    );
+    return res.status(200).json(result.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen("4000", (req, res) => {
   console.log("Server listening on port 4000");
