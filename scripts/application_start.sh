43dfc4417e40e94317a3ad8e419a45b4a8be23b8
THANH <95860402+ccna123@users.noreply.github.com>
2024-04-23 11:00:12 +0900

    update

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-#install pm2
-sudo npm install -g pm2 -y
-
 #go to app working directory
 chmod 700 -R harrypoter/
+sudo chown -R ec2-user:ec2-user harrypoter/
 cd /home/ec2-user/harrypoter
 
 #add npm and node to path so that we can execute npm from working directory
@@ -13,4 +11,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 sudo npm install
+npm install -g pm2
 pm2 start index.js
