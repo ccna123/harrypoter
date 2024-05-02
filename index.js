93db31454dfe71c4ab8422e8eff5dc816295f54b
THANH <95860402+ccna123@users.noreply.github.com>
2024-05-02 19:51:36 +0900

    update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { default: axios } = require("axios");
 const express = require("express");
 const serverless = require("serverless-http");
+require("dotenv").config();
 
 const charRoute = require("./chars");
 const spellRoute = require("./spells");
 const bookRoute = require("./books");
 
 const app = express();
-
 app.get("/health", (req, res) => {
   res.status(200).send({
     mess: "Success",
@@ -18,8 +18,10 @@ app.use("/harrypotter", charRoute);
 app.use("/harrypotter", spellRoute);
 app.use("/harrypotter", bookRoute);
 
-// app.listen("4000", (req, res) => {
-//   console.log("Server listening on port 4000");
-// });
-
-module.exports.handler = serverless(app);
+if (process.env.ENVIRONMENT === "lambda") {
+  module.exports.handler = serverless(app);
+} else {
+  app.listen("4000", (req, res) => {
+    console.log("Server listening on port 4000");
+  });
+}
